d087c7942cbe50b2f596586b6a378802497d3d6e
Alexander Butusov <alexander.kiosk@gmail.com>
2022-02-08 20:30:06 +0300

    BubleSort Task 2

---
+++ CourseApp/Module2/BubbleSortPair.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseApp.Module2
+{
+    public class BubbleSortPair
+    {
+        public static void BubbleSortMethod()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] number = new int[n];
+            int[] price = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                string s = Console.ReadLine();
+                string[] sValues = s.Split(' ');
+                number[i] = int.Parse(sValues[0]);
+                price[i] = int.Parse(sValues[1]);
+            }
+
+            for (int i = 0; i < price.Length - 1; i++)
+            {
+                for (int j = 0; j < price.Length - i - 1; j++)
+                {
+                    if (price[j] < price[j + 1])
+                    {
+                        (price[j], price[j + 1]) = (price[j + 1], price[j]);
+                        (number[j], number[j + 1]) = (number[j + 1], number[j]);
+                    }
+                    else if (price[j] == price[j + 1])
+                    {
+                        if (number[j] > number[j + 1])
+                        {
+                            (number[j], number[j + 1]) = (number[j + 1], number[j]);
+                            (price[j], price[j + 1]) = (price[j + 1], price[j]);
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0} {1}", number[i], price[i]);
+            }
+        }
+    }
+}

04992ece928e50faa535af376646ec97d0e0d20a
HubOl01 <kuloleg52@gmail.com>
2022-02-09 13:46:50 +0300

    add name

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorythm_Sharp_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

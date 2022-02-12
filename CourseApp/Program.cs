670507ec757f75d9793bbf11c7ffbdf162941408
Владимир Соколов <dantedmc.5@mail.ru>
2022-02-12 15:31:17 +0300

    uodate

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -7,7 +7,8 @@ namespace CourseApp
     {
         public static void Main(string[] args)
         {
-            BubbleSort.Bubble_sort();
+            // BubbleSort.Bubble_sort(); //
+            BubbleSort2.BubbleSort_2();
         }
     }
 }

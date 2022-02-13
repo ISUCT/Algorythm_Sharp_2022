4ed9b0468ea0d30b0aa05a2a5099a01f8981da58
Egoro Vitaly <egorov21112061vito@gmail.com>
2022-02-13 12:07:33 +0300

    Added Bubble Pair Sort

---
+++ CourseApp.Tests/Module2/BubblePairSortTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using Xunit;
+using CourseApp.Module2;
+
+namespace CourseApp.Tests.Module2
+{
+    [Collection("Sequential")]
+    public class BubblePairSortTest : IDisposable
+    {
+        private const string Inp1 = @"3
+190 76
+299 86
+191 26";
+
+        private const string Out1 = @"299 86
+190 76
+191 26";
+
+        private const string Inp2 = @"3
+1 3
+3 4
+2 4";
+
+        private const string Out2 = @"2 4
+3 4
+1 3";
+
+        public void Dispose()
+        {
+            var standardOut = new StreamWriter(Console.OpenStandardOutput());
+            standardOut.AutoFlush = true;
+            var standardIn = new StreamReader(Console.OpenStandardInput());
+            Console.SetOut(standardOut);
+            Console.SetIn(standardIn);
+        }
+
+        [Theory]
+        [InlineData(Inp1, Out1)]
+        [InlineData(Inp2, Out2)]
+        public void Test1(string input, string expected)
+        {
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            var stringReader = new StringReader(input);
+            Console.SetIn(stringReader);
+
+            // act
+            BubblePairSort.BubblePairSortMethod();
+
+            // assert
+            var output = stringWriter.ToString().Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            var result = string.Join(Environment.NewLine, output);
+            Assert.Equal($"{expected}", result);
+        }
+    }
+}

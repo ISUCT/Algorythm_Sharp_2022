4f41db88e2dad9e5edc81dcef4bcc87bc4bc0540
Gv1don <crgvidonlol@gmail.com>
2022-02-13 16:54:42 +0300

    Added solution for PairSort-task and test for this solution

---
+++ CourseApp/Module2/PairSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class PairSort
+{
+    public static void PairSortMethod()
+    {
+        int numberOfElements = int.Parse(Console.ReadLine());
+        string[][] arrayOfElements = new string[numberOfElements][];
+
+        for (int i = 0; i < numberOfElements; i++)
+        {
+            string[] value = Console.ReadLine().Split(new char[] { ' ' });
+            arrayOfElements[i] = value;
+        }
+
+        for (int l = 0; l < numberOfElements - 1; l++)
+        {
+            for (int k = 0; k < numberOfElements - 1; k++)
+            {
+                if (Convert.ToInt32(arrayOfElements[k][1]) < Convert.ToInt32(arrayOfElements[k + 1][1]))
+                {
+                    (arrayOfElements[k], arrayOfElements[k + 1]) = (arrayOfElements[k + 1], arrayOfElements[k]);
+                }
+                else if (arrayOfElements[k][1] == arrayOfElements[k + 1][1])
+                {
+                    if (Convert.ToInt32(arrayOfElements[k][0]) > Convert.ToInt32(arrayOfElements[k + 1][0]))
+                    {
+                        (arrayOfElements[k], arrayOfElements[k + 1]) = (arrayOfElements[k + 1], arrayOfElements[k]);
+                    }
+                }
+            }
+        }
+
+        for (int p = 0; p < numberOfElements; p++)
+        {
+            string result = string.Join(" ", arrayOfElements[p]);
+            Console.WriteLine(result);
+        }
+
+        Console.WriteLine();
+    }
+}
--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -4,7 +4,8 @@
     {
         private static void Main()
         {
-            BubbleSort.BubbleSortMethod();
+            // BubbleSort.BubbleSortMethod();
+            PairSort.PairSortMethod();
         }
     }
 }

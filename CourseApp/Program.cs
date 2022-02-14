2d78f13d7ba22dc2d0996cfd7e1afccf5d9139cd
semen.pigolitsyn <semen23112002@mail.ru>
2022-02-14 08:27:11 +0300

    Add PairSort and tests

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -7,7 +7,6 @@ namespace CourseApp
     {
         public static void Main(string[] args)
         {
-            BubbleSort.BubbleSortMethod();
             Console.WriteLine("Hello World");
         }
     }

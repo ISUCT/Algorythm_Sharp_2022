45c4fac55b5a896241f66a898e412d348478ca71
ARBUZZZ228 <alecksey17@icloud.com>
2022-02-14 18:46:12 +0300

    feat: add pair sort

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -7,7 +7,7 @@ namespace CourseApp
     {
         public static void Main(string[] args)
         {
-            BubbleSort.BubbleSortMethod();
+            PairSort.PairSortMethod();
         }
     }
 }

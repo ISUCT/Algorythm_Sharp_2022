e489913a5855b70ffb14e681c65d136b6b96a709
EwvwGeN <doj54567467z@gmail.com>
2022-02-15 18:24:07 +0300

    again...

--- CourseApp.Tests/Module2/PairSortTest.cs
+++ CourseApp.Tests/Module2/PairSortTest.cs
@@ -7,7 +7,7 @@ using CourseApp.Module2;
 namespace CourseApp.Tests.Module2
 {
     [Collection("Sequential")]
-    public class PairSortTest
+    public class PairSortTest : IDisposable
     {
         private const string Inp1 = @"101 80
 305 90
@@ -25,15 +25,24 @@ namespace CourseApp.Tests.Module2
 25 90
 30 90";
 
+        public void Dispose()
+        {
+            var standardOut = new StreamWriter(Console.OpenStandardOutput());
+            standardOut.AutoFlush = true;
+            var standardIn = new StreamReader(Console.OpenStandardInput());
+            Console.SetOut(standardOut);
+            Console.SetIn(standardIn);
+        }
+
         [Theory]
         [InlineData(Inp1, Out1)]
         [InlineData(Inp2, Out2)]
         public void Checking_PairSort_Works_Correctly(string input, string expected)
         {
             // act
-            string[] testingData = input.Split("\r\n").ToArray();
+            string[] testingData = input.Split(Environment.NewLine).ToArray();
 
-            string answer = string.Join("\r\n", PairSort.PairBubbleSortMethod(testingData));
+            string answer = string.Join(Environment.NewLine, PairSort.PairBubbleSortMethod(testingData));
 
             // assert
             Assert.Equal($"{expected}", answer);

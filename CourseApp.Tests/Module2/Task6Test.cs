f0479116b96fdae19181946159bb98004039d7cd
fixat0r <snikita.vinogradov.54@mail.ru>
2022-03-04 00:37:50 +0300

    Task6_Test

---
+++ CourseApp.Tests/Module2/Task6Test.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using Xunit;
+using CourseApp.Module2;
+
+namespace CourseApp.Tests.Module2
+{
+    [Collection("Sequential")]
+    public class Task6Test : IDisposable
+    {
+        private const string Inp1 = @"5
+1 50 3 4 3
+16
+1 2 3 4 5 1 3 3 4 5 5 5 5 5 4 5";
+
+        private const string Inp2 = @"3
+1 2 3
+4
+1 2 2 3
+";
+
+        private const string Out1 = @"yes
+no
+no
+no
+yes";
+
+        private const string Out2 = @"no
+no
+no";
+
+        public void Dispose()
+        {
+            var standardOut = new StreamWriter(Console.OpenStandardOutput());
+            standardOut.AutoFlush = true;
+            var standardIn = new StreamReader(Console.OpenStandardInput());
+            Console.SetOut(standardOut);
+            Console.SetIn(standardIn);
+        }
+
+        [Theory]
+        [InlineData(Inp1, Out1)]
+        [InlineData(Inp2, Out2)]
+        public void Test1(string input, string expected)
+        {
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            var stringReader = new StringReader(input);
+            Console.SetIn(stringReader);
+
+            // act
+            Task6.Task6_Main();
+
+            // assert
+            var output = stringWriter.ToString().Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            var result = string.Join(Environment.NewLine, output);
+            Assert.Equal($"{expected}", result);
+        }
+    }
+}

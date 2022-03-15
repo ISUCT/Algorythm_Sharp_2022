534e9353bd5f9eb1415fe2d080bb84694738d63d
Violettam88 <violettam88@yandex.ru>
2022-03-15 18:13:44 +0300

    done 2.1 with unit test

--- CourseApp.Tests/Module2/BubbleSortTest.cs
+++ CourseApp.Tests/Module2/BubbleSortTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Text;
 using Xunit;
 using CourseApp.Module2;
 
@@ -8,11 +10,27 @@ namespace CourseApp.Tests.Module2
     [Collection("Sequential")]
     public class BubbleSortTest : IDisposable
     {
-        private const string Inp1 = @"7
-5 1 7 3 9 4 1";
+        private const string Inp1 = @"5
+4 3 2 1 10";
 
-        private const string Inp2 = @"3
--10 7 2";
+        private const string Inp2 = @"4
+4 3 2 1";
+
+        private const string Out1 = @"3 4 2 1 10
+3 2 4 1 10
+3 2 1 4 10
+2 3 1 4 10
+2 1 3 4 10
+1 2 3 4 10
+";
+
+        private const string Out2 = @"3 4 2 1
+3 2 4 1
+3 2 1 4
+2 3 1 4
+2 1 3 4
+1 2 3 4
+";
 
         public void Dispose()
         {
@@ -24,22 +42,22 @@ namespace CourseApp.Tests.Module2
         }
 
         [Theory]
-        [InlineData(Inp1, "1 1 3 4 5 7 9")]
-        [InlineData(Inp2, "-10 2 7")]
-        public void Test1(string input, string expected)
+        [InlineData(Inp1, Out1)]
+        [InlineData(Inp2, Out2)]
+        public void Check_BubbleSort(string input, string expected)
         {
-            var stringWriter = new StringWriter();
-            Console.SetOut(stringWriter);
-
-            var stringReader = new StringReader(input);
-            Console.SetIn(stringReader);
-
             // act
-            BubbleSort.BubbleSortMethod();
+            StreamWriter write = new StreamWriter("input.txt");
+            write.WriteLine(input);
+            write.Close();
+
+            BubbleSort.DoBubbleSort();
 
             // assert
-            var output = stringWriter.ToString().Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
-            Assert.Equal($"{expected}", output[0]);
+            var output = File.ReadAllText("output.txt");
+            Assert.Equal(expected, output);
+            File.Delete("input.txt");
+            File.Delete("output.txt");
         }
     }
 }

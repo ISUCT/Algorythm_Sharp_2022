b0c0983e3f9acd7044505d29cba130396408e8f7
ARBUZZZ228 <alecksey17@icloud.com>
2022-03-25 00:14:01 +0300

    Add 3.4 and Test

---
+++ CourseApp/Module3/CyclicString.cs
@@ -0,0 +1,33 @@
+﻿namespace CourseApp.Module3
+{
+    using System;
+
+    public class CyclicString
+    {
+        public static void CyclicStringMethod()
+        {
+            string line = Console.ReadLine();
+            int[] f = new int[line.Length];
+            f[0] = 0;
+            for (int i = 0; i < line.Length - 1; i++)
+            {
+                int current = f[i];
+                while (current > 0 && line[i + 1] != line[current])
+                {
+                    current = f[current - 1];
+                }
+
+                if (line[i + 1] == line[current])
+                {
+                    f[i + 1] = current + 1;
+                }
+                else
+                {
+                    f[i + 1] = 0;
+                }
+            }
+
+            Console.WriteLine(line.Length - f[line.Length - 1]);
+        }
+    }
+}

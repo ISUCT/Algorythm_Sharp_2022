43d64f3f9cc732f68852254610ef2e14eb01814a
ARBUZZZ228 <alecksey17@icloud.com>
2022-03-24 21:22:09 +0300

    Add 3.3 and Test

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -7,7 +7,7 @@ namespace CourseApp
     {
         public static void Main(string[] args)
         {
-            RabinCarp.RabinCarpMethod();
+            PeriodString.PeriodStringMethod();
         }
     }
 }

4b10120a4c3cc4c78892343e183cfbe507a47417
CuteDarKy <1705zaq@gmail.com>
2022-04-15 15:33:39 +0300

    Module 2 Task 1 (BubbleSort)

--- CourseApp/Module2/BubbleSort.cs
+++ CourseApp/Module2/BubbleSort.cs
@@ -8,31 +8,34 @@ namespace CourseApp.Module2
     {
         public static void BubbleSortMethod()
         {
-            int n = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine()); //чтобы нужный тип - parse
             string s = Console.ReadLine();
-            string[] sValues = s.Split(' ');
+            string[] sValues = s.Split(' '); //массив sValues разделяем на части (Values - ценности), он не понимает, что это отдельные числа
             int[] arr = new int[n];
             for (int i = 0; i < n; i++)
             {
-                arr[i] = int.Parse(sValues[i]);
+                arr[i] = int.Parse(sValues[i]); //в инт из строки, присваиваем числа ввода в осн массив
             }
 
-            for (int i = 0; i < arr.Length - 1; i++)
+            bool swap = false;
+            for (int i = 0; i < arr.Length - 1; i++)//(1/2) это + 
             {
-                for (int j = 0; j < arr.Length - i - 1; j++)
+                for (int j = 0; j < arr.Length - i - 1; j++) //(2/2) это вот  - сама сортировка из алгоритма (см. презенташку)
                 {
-                    if (arr[j] > arr[j + 1])
+                    if (arr[j] > arr[j + 1]) // 1й эл-т сравниваем со 2м
                     {
-                        // int temp = arr[j];
-                        // arr[j] = arr[j + 1];
-                        // arr[j+1] = temp;
-                        (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+                        swap = true;
+                        (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]); //меняем местами
+                        string result = string.Join(" ", arr); // join вводит " " м/у эл-тами массива
+                        Console.WriteLine(result);
                     }
                 }
             }
 
-            string result = string.Join(" ", arr);
-            Console.WriteLine(result);
+            if (swap == false)
+            {
+                Console.WriteLine(0);
+            }
         }
     }
 }
